29d8dca2c2d1ac55d0337eeacca925c9ea15ee0f
Mike The Great Developer <mikeolams@yahoo.com>
2018-12-06 01:49:01 +0100

    add login-page.js controller

---
+++ src/controllers/login-page.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myApp3App.controller:YuCtrl
+ * @description
+ * # YuCtrl
+ * Controller of the myApp3App
+ */
+angular.module('rent')
+  .controller('Login-pageCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
